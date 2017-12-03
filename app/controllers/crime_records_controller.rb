793c5e342bc51308a5be9ebe895bcaf12f203060
carlaraya <carl_angelo.araya@upd.edu.ph>
2017-12-03 14:33:17 +0800

    Finish crime record crud

--- app/controllers/crime_records_controller.rb
+++ app/controllers/crime_records_controller.rb
@@ -1,11 +1,13 @@
 class CrimeRecordsController < ApplicationController
-  before_action :set_crime_record, only: [:show, :update, :destroy]
+  before_action :set_crime_record_police_station, only: [:show, :update, :destroy]
+  before_action :authenticate_request, only: [:update, :destroy]
+  before_action :auth_request_any, only: [:create]
 
   # GET /crime_records
   def index
     @crime_records = CrimeRecord.all
 
-    render json: @crime_records
+    render json: @crime_records, except: [:created_at, :updated_at]
   end
 
   # GET /crime_records/1
@@ -16,6 +18,7 @@ class CrimeRecordsController < ApplicationController
   # POST /crime_records
   def create
     @crime_record = CrimeRecord.new(crime_record_params)
+    @crime_record.police_station = @current_police_station
 
     if @crime_record.save
       render json: @crime_record, status: :created, location: @crime_record
@@ -40,12 +43,13 @@ class CrimeRecordsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_crime_record
+    def set_crime_record_police_station
       @crime_record = CrimeRecord.find(params[:id])
+      @police_station = @crime_record.police_station
     end
 
     # Only allow a trusted parameter "white list" through.
     def crime_record_params
-      params.require(:crime_record).permit(:crime_type_id, :police_station_id, :new, :solved, :is_hidden)
+      params.require(:crime_record).permit(:crime_type_id, :new, :solved, :is_hidden)
     end
 end
