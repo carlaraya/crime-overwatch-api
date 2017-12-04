204f5d1d19e4ba4be4a700555cd5331620c2e418
carlaraya <carl_angelo.araya@upd.edu.ph>
2017-12-04 15:34:07 +0800

    Change seeds, send id along w/ auth token

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,7 +2,7 @@ class AuthenticationController < ApplicationController
   def authenticate
     command = AuthenticatePoliceStation.call(params[:username], params[:password])
     if command.success?
-      render json: { auth_token: command.result }
+      render json: { auth_token: command.result, id: PoliceStation.find_by_username(params[:username]).id }
     else
       render json: { error: command.errors }, status: :unauthorized
     end
